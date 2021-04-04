7fd41165e4289f4135f04608bfa180a9088e7dbb
T.A. Ma <anhmt90@gmail.com>
2021-04-04 12:23:55 +0200

    adapt oracle client to async oracle requests and responses

--- contracts/PassiveInvestment.sol
+++ contracts/PassiveInvestment.sol
@@ -3,52 +3,85 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IndexToken.sol";
+import "./oracle/IOracleClient.sol";
+import "./oracle/Oracle.sol";
 
-contract PassiveInvestment is Ownable {
-    // address payable private owner; // admin address should NOT be exposed
+contract PassiveInvestment is Ownable, IOracleClient {
+    struct Purchase {
+        uint256 _id;
+        address _buyer;
+        uint256 _numberOfTokens;
+        uint256 _price;
+    }
+
+    uint256 constant MAX_UINT256 = uint256(-1);
 
     // address of the ERC20 IndexToken contract
-    IndexToken public tokenContract;
+    IndexToken private tokenContract = IndexToken(address(0));
+    Oracle private oracleContract = Oracle(address(0));
+
+    mapping(uint256 => Purchase) pendingPurchases;
 
     // <token_itin> is set at <price> in Ether
-    mapping(bytes32 => uint256) public portfolio;
-
-    // current price
-    uint256 public tokenPrice;
+    // mapping(bytes32 => uint256) public portfolio;
 
     // keep track of the token amount sold out to the market
     uint256 public tokensSold;
 
-    event Sell(address _buyer, uint256 _amount);
+    event Sale(
+        uint256 indexed _reqId,
+        address indexed _buyer,
+        uint256 _amount,
+        uint256 _price
+    );
+    event PriceRequest(uint256 indexed _reqId, address indexed _buyer);
+    event PurchaseReady(uint256 indexed _reqId, address indexed _buyer);
 
-    constructor(IndexToken _tokenContract, uint256 _tokenPrice) {
+    constructor(IndexToken _tokenContract, Oracle _oracleContract) {
         tokenContract = _tokenContract;
-        tokenPrice = _tokenPrice;
+        oracleContract = _oracleContract;
     }
 
     // payable: function can exec Tx
-    function buyTokens(uint256 _numberOfTokens) public payable {
-        // require that the calling entity has enough funds to buy tokens
-        require(
-            msg.value == (_numberOfTokens * tokenPrice),
-            "Not enough funds to buy tokens"
+    function orderTokens(uint256 _numberOfTokens) public {
+        uint256 _reqId = assignRequestId();
+
+        pendingPurchases[_reqId] = Purchase(
+            _reqId,
+            msg.sender,
+            _numberOfTokens,
+            MAX_UINT256
         );
+
+        oracleContract.request(_reqId);
+
+        emit PriceRequest(_reqId, msg.sender);
+    }
+
+    function purchase(uint256 _reqId) public payable returns (bool)  {
+        // require that the request Id passed in is available
+        require(pendingPurchases[_reqId]._id != 0, "Request ID not found");
+
+        // require that the function caller is the buyer that placed token order earlier
+        require(pendingPurchases[_reqId]._buyer == msg.sender, "Unauthorized purchase claim");
 
         // require that the contract has enough tokens
-        require(
-            tokenContract.balanceOf(address(this)) >= _numberOfTokens,
-            "Unable to purchase more tokens than totally available"
-        );
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens, "Unable to purchase more tokens than totally available");
+
+        // require that actual price has been queried and received from the oracle
+        require(pendingPurchases[_reqId]._price != MAX_UINT256, "Price is yet to set");
+
+        // require that the calling entity has enough funds to buy tokens
+        require(msg.value == (pendingPurchases[_reqId]._numberOfTokens * pendingPurchases[_reqId]._price), "Not enough funds to buy tokens");
 
         // require that a transfer is successful
-        require(
-            tokenContract.transfer(msg.sender, _numberOfTokens),
-            "Unable to transfer tokens to buyer"
-        );
+        require(tokenContract.transfer(msg.sender, _numberOfTokens), "Unable to transfer tokens to buyer");
 
         tokensSold += _numberOfTokens;
 
-        emit Sell(msg.sender, _numberOfTokens);
+        emit Sale(msg.sender, pendingPurchases[_reqId]._numberOfTokens, pendingPurchases[_reqId]._price);
+
+        return true;
     }
 
     //Ending Token DappTokenSale
@@ -67,5 +100,18 @@ contract PassiveInvestment is Ownable {
         // it's immutable. But the contract will be `disable` and its state variables
         // will be set the default value of their datatype.
         selfdestruct(payable(owner()));
+    }
+
+    // @notice a callback for Oracle contract to call once the requested data is ready
+    function oracleCallback(uint256 _reqId, uint256 _price)
+        external
+        override
+        returns (bool)
+    {
+        Purchase memory _purchase = pendingPurchases[_reqId];
+        _purchase._price = _price;
+        delete pendingPurchases[_reqId];
+
+        emit PurchaseReady(_reqId, _buyer);
     }
 }
