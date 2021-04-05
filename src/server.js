66c6966bb955d576ccae67dbae1857e1cd71c6db
T.A. Ma <anhmt90@gmail.com>
2021-04-05 15:42:47 +0200

    oracle server to subscribe to PriceRequest event

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
-const fs = require('fs');
-const path = require('path');
-
 // const ganache = require('ganache-cli');
 // const web3 = new Web3(ganache.provider());
 
 const Web3 = require('web3');
-const web3 = new Web3("http://localhost:8545");
+// const web3 = new Web3("http://localhost:8545");
 
-const TOKEN_CONTRACT =  require('../build/contracts/IndexToken.json');
-const ORACLE_CONTRACT = require('../build/contracts/Oracle.json');
-const INVESTMENT_CONTRACT =  require('../build/contracts/PassiveInvestment.json');
+// Ref.: https://hanezu.net/posts/Enable-WebSocket-support-of-Ganache-CLI-and-Subscribe-to-Events.html
+const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'));
+
+const TOKEN_CONTRACT_JSON = require('../build/contracts/IndexToken.json');
+const ORACLE_CONTRACT_JSON = require('../build/contracts/Oracle.json');
+const INVESTMENT_CONTRACT_JSON = require('../build/contracts/PassiveInvestment.json');
 
 let admin;
 let trustedOracleServer;
@@ -21,23 +21,23 @@ const deployTokenContract = async () => {
 
     let contractAddress;
     let costPaid;
-    const tokenContractInstance = new web3.eth.Contract(TOKEN_CONTRACT.abi);
+    const tokenContractInstance = new web3.eth.Contract(TOKEN_CONTRACT_JSON.abi);
     await tokenContractInstance.deploy({
-        data: TOKEN_CONTRACT.bytecode,
+        data: TOKEN_CONTRACT_JSON.bytecode,
         arguments: ["100000"]
     })
-    .send({
-        from: admin,
-        gas: '3000000'
-    })
-    .on('receipt', async (txReceipt) => {
-        if(txReceipt.contractAddress) {
-            contractAddress = txReceipt.contractAddress;
-            console.log("Deployed at address: ", contractAddress);
-        }
-        // costPaid = txReceipt.gasUsed * web3.utils.fromWei(await web3.eth.getGasPrice(), 'ether');
-        console.log("Gas used (token): ", txReceipt.gasUsed);
-    })
+        .send({
+            from: admin,
+            gas: '3000000'
+        })
+        .on('receipt', async (txReceipt) => {
+            if (txReceipt.contractAddress) {
+                contractAddress = txReceipt.contractAddress;
+                console.log("Deployed at address: ", contractAddress);
+            }
+            // costPaid = txReceipt.gasUsed * web3.utils.fromWei(await web3.eth.getGasPrice(), 'ether');
+            console.log("Gas used (token): ", txReceipt.gasUsed);
+        });
 
     return contractAddress;
 };
@@ -49,24 +49,24 @@ const deployOracleContract = async () => {
     console.log("Using trusted Oracle server: ", trustedOracleServer);
 
     let contractAddress;
-    const oracleContractInstance = new web3.eth.Contract(ORACLE_CONTRACT.abi);
+    const oracleContractInstance = new web3.eth.Contract(ORACLE_CONTRACT_JSON.abi);
     await oracleContractInstance.deploy({
-        data: ORACLE_CONTRACT.bytecode,
+        data: ORACLE_CONTRACT_JSON.bytecode,
         arguments: [trustedOracleServer]
     })
-    .send({
-        from: admin,
-        gas: '3000000'
-    })
-    .on('receipt', async (txReceipt) => {
-        if(txReceipt.contractAddress) {
-            contractAddress = txReceipt.contractAddress;
-            console.log("Deployed at address: ", contractAddress);
-        }
+        .send({
+            from: admin,
+            gas: '3000000'
+        })
+        .on('receipt', async (txReceipt) => {
+            if (txReceipt.contractAddress) {
+                contractAddress = txReceipt.contractAddress;
+                console.log("Deployed at address: ", contractAddress);
+            }
 
-        console.log("Gas used (oracle): ", txReceipt.gasUsed);
+            console.log("Gas used (oracle): ", txReceipt.gasUsed);
 
-    })
+        });
 
     return contractAddress;
 };
@@ -77,24 +77,24 @@ const deployInvestmentContract = async (tokenContractAddress, oracleContractAddr
     console.log("Using account: ", admin);
 
     let contractAddress;
-    const investmentContractInstance = new web3.eth.Contract(INVESTMENT_CONTRACT.abi);
+    const investmentContractInstance = new web3.eth.Contract(INVESTMENT_CONTRACT_JSON.abi);
     await investmentContractInstance.deploy({
-        data: INVESTMENT_CONTRACT.bytecode,
+        data: INVESTMENT_CONTRACT_JSON.bytecode,
         arguments: [tokenContractAddress, oracleContractAddress]
     })
-    .send({
-        from: admin,
-        gas: '3000000'
-    })
-    .on('receipt', async (txReceipt) => {
-        if(txReceipt.contractAddress) {
-            contractAddress = txReceipt.contractAddress;
-            console.log("Deployed at address: ", contractAddress);
-        }
-        console.log("Gas used (investment): ", txReceipt.gasUsed);
-    })
+        .send({
+            from: admin,
+            gas: '3000000'
+        })
+        .on('receipt', async (txReceipt) => {
+            if (txReceipt.contractAddress) {
+                contractAddress = txReceipt.contractAddress;
+                console.log("Deployed at address: ", contractAddress);
+            }
+            console.log("Gas used (investment): ", txReceipt.gasUsed);
+        });
     return contractAddress;
-}
+};
 
 (async () => {
     const accounts = await web3.eth.getAccounts();
@@ -110,5 +110,42 @@ const deployInvestmentContract = async (tokenContractAddress, oracleContractAddr
     const investmentContractAddress = await deployInvestmentContract(tokenContractAddress, oracleContractAddress);
     console.log("Investment contract at: ", investmentContractAddress);
 
+
+    const oracleContractInstance = new web3.eth.Contract(ORACLE_CONTRACT_JSON.abi, oracleContractAddress);
+    await oracleContractInstance.methods.addClient(investmentContractAddress).send({
+        from: admin,
+        gas: '3000000'
+    });
+
+    /**
+     * Subscribe to events
+     * Resources:
+     *      https://web3js.readthedocs.io/en/v1.2.0/web3-eth-contract.html#id36
+     *      https://hanezu.net/posts/Enable-WebSocket-support-of-Ganache-CLI-and-Subscribe-to-Events.html
+     *      https://ethereum.stackexchange.com/questions/35997/how-to-listen-to-events-using-web3-v1-0
+     *      https://betterprogramming.pub/ethereum-dapps-how-to-listen-for-events-c4fa1a67cf81
+     */
+    oracleContractInstance.events.RequestPrice({})
+        .on('data', function (event) {
+            console.log("GOT ", event);
+        })
+        .on('changed', function (event) {
+            console.log(event, " removed");
+        })
+        .on('error', console.error);
+
 })();
 
+
+
+   // let web3Provider = new Web3.providers.WebsocketProvider("wss://ropsten.infura.io/ws");
+    // var web3Obj = new Web3(web3Provider);
+    // var subscription = web3Obj.eth.subscribe('logs', {
+    //     address: '0x123456..', //Smart contract address
+    //     topics: ['0x12345...'] //topics for events
+    // }, function(error, result){
+    //     if (error) console.log(error);
+    // }).on("data", function(trxData){
+    // console.log("Event received", trxData);
+    // //Code from here would be run immediately when event appeared
+    // }))
