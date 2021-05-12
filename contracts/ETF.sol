8f1fee3267d97c5fa58c94a54d0ec66100cba141
T.A. Ma <anhmt90@gmail.com>
2021-05-12 23:37:10 +0200

    fix bug not set portfolio leading to transaction gets reverted

--- contracts/ETF.sol
+++ contracts/ETF.sol
@@ -5,7 +5,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
-import "@uniswap/v2-periphery/contracts/interfaces/IWETH.sol";
+// import "@uniswap/v2-periphery/contracts/interfaces/IWETH.sol";
+import "./interfaces/IWETH.sol";
 import "./interfaces/IERC20Extended.sol";
 import "./IndexToken.sol";
 import "./oracle/IOracleClient.sol";
@@ -34,7 +35,7 @@ contract ETF is Ownable, IOracleClient {
     IUniswapV2Router02 public router = IUniswapV2Router02(address(0));
 
     // instance of WETH
-    address public weth = address(0);
+    IWETH public weth = IWETH(address(0));
 
     // set of pending purchases that are yet to finalize
     mapping(uint256 => Purchase) pendingPurchases;
@@ -70,7 +71,7 @@ contract ETF is Ownable, IOracleClient {
         tokenContract = _tokenContract;
         factory = _factory;
         router = _router;
-        weth = _weth;
+        weth = IWETH(_weth);
     }
 
     function setPorfolio(string[] memory names, address[] memory addresses)
@@ -81,8 +82,8 @@ contract ETF is Ownable, IOracleClient {
             names.length == addresses.length,
             "ETF/Arrays not equal in length!"
         );
+        tokenNames = names;
         for (uint256 i = 0; i < names.length; i++) {
-            tokenNames[i] = names[i];
             portfolio[names[i]] = addresses[i];
         }
         emit PortfolioChanged(names, addresses);
@@ -101,74 +102,9 @@ contract ETF is Ownable, IOracleClient {
 
         finalize(_reqId);
 
-        oracleContract.request(_reqId);
+        // oracleContract.request(_reqId);
 
         // emit PriceRequest(_reqId, msg.sender);
-    }
-
-    // @notice a callback for Oracle contract to call once the requested data is ready
-    function __oracleCallback(uint256 _reqId, uint256 _price)
-        external
-        override
-        returns (bool)
-    {
-        require(pendingPurchases[_reqId]._id != 0, "Request ID not found");
-
-        pendingPurchases[_reqId]._price = _price;
-
-        emit PurchaseReady(_reqId, pendingPurchases[_reqId]._buyer, _price);
-
-        return true;
-    }
-
-    function getTokenPrice(address pairAddress)
-        public
-        view
-        returns (uint256 price)
-    {
-        IUniswapV2Pair pair = IUniswapV2Pair(pairAddress);
-        address token0Addr = pair.token0();
-        address token1Addr = pair.token1();
-        require(
-            weth == token0Addr || weth == token1Addr,
-            "ETF/Not a ERC20 <-> WETH pair"
-        );
-
-        IERC20Extended token0 = IERC20Extended(token0Addr);
-        IERC20Extended token1 = IERC20Extended(token1Addr);
-        (uint256 Res0, uint256 Res1, ) = pair.getReserves();
-
-        // return amount of WETH needed to buy one token1
-        if (token0Addr == weth) {
-            price = ((Res0 * (10**token1.decimals())) / Res1);
-        } else {
-            price = ((Res1 * (10**token0.decimals())) / Res0);
-        }
-    }
-
-    function swapExactETHForTokens() internal {
-        require(
-            portfolio[tokenNames[0]] != address(0),
-            "ETF/DAI Token not set"
-        );
-
-        string memory tokenName = tokenNames[0];
-        address tokenAddr = portfolio[tokenName];
-        address[] memory path;
-        path[0] = weth;
-        path[1] = tokenAddr;
-
-        IERC20Extended token = IERC20Extended(tokenAddr);
-
-        uint256[] memory amounts =
-            router.swapExactETHForTokens(
-                1 * (10**token.decimals()),
-                path,
-                address(this),
-                block.timestamp + 10
-            );
-
-        emit Swap(amounts);
     }
 
     function finalize(uint256 _reqId) internal returns (bool) {
@@ -190,10 +126,8 @@ contract ETF is Ownable, IOracleClient {
 
         // require that actual price has been queried and received from the oracle
         // require(pendingPurchases[_reqId]._price != MAX_UINT256, "Price is yet to set");
-
-        address pairAddress = factory.getPair(weth, portfolio[tokenNames[0]]);
+        address pairAddress = factory.getPair(address(weth), portfolio[tokenNames[0]]);
         pendingPurchases[_reqId]._price = getTokenPrice(pairAddress);
-        // pendingPurchases[_reqId]._price = 10**18;
 
         // require that the calling entity has enough funds to buy tokens
         require(msg.value >= (pendingPurchases[_reqId]._numberOfTokens * pendingPurchases[_reqId]._price),
@@ -222,6 +156,72 @@ contract ETF is Ownable, IOracleClient {
 
         return true;
     }
+
+    // @notice a callback for Oracle contract to call once the requested data is ready
+    function __oracleCallback(uint256 _reqId, uint256 _price)
+        external
+        override
+        returns (bool)
+    {
+        require(pendingPurchases[_reqId]._id != 0, "Request ID not found");
+
+        pendingPurchases[_reqId]._price = _price;
+
+        emit PurchaseReady(_reqId, pendingPurchases[_reqId]._buyer, _price);
+
+        return true;
+    }
+
+    function getTokenPrice(address pairAddress)
+        public
+        view
+        returns (uint256 price)
+    {
+        IUniswapV2Pair pair = IUniswapV2Pair(pairAddress);
+        address token0Addr = pair.token0();
+        address token1Addr = pair.token1();
+        require(
+            address(weth) == token0Addr || address(weth) == token1Addr,
+            "ETF/Not a ERC20 <-> WETH pair"
+        );
+
+        IERC20Extended token0 = IERC20Extended(token0Addr);
+        IERC20Extended token1 = IERC20Extended(token1Addr);
+        (uint256 Res0, uint256 Res1, ) = pair.getReserves();
+
+        // return amount of WETH needed to buy one token1
+        if (token0Addr == address(weth)) {
+            price = ((Res0 * (10**token1.decimals())) / Res1);
+        } else {
+            price = ((Res1 * (10**token0.decimals())) / Res0);
+        }
+    }
+
+    function swapExactETHForTokens() internal {
+        require(
+            portfolio[tokenNames[0]] != address(0),
+            "ETF/DAI Token not set"
+        );
+
+        string memory tokenName = tokenNames[0];
+        address tokenAddr = portfolio[tokenName];
+        address[] memory path = new address[](tokenNames.length + 1);
+        path[0] = address(weth);
+        path[1] = tokenAddr;
+
+        IERC20Extended token = IERC20Extended(tokenAddr);
+
+        uint256[] memory amounts =
+            router.swapExactETHForTokens{value: msg.value}(
+                1 * (10**token.decimals()),
+                path,
+                address(this),
+                block.timestamp + 10
+            );
+
+        emit Swap(amounts);
+    }
+
 
     //Ending Token DappTokenSale
     function endSale() public onlyOwner {
