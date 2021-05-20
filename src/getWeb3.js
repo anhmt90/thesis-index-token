80fcb09f58c330ff9ef280500ea4b37d715c64e7
T.A. Ma <anhmt90@gmail.com>
2021-05-20 17:32:29 +0100

    distinguish web3 for production and testing

--- src/getWeb3.js
+++ src/getWeb3.js
@@ -5,8 +5,17 @@
 
 // Ref.: https://hanezu.net/posts/Enable-WebSocket-support-of-Ganache-CLI-and-Subscribe-to-Events.html
 
+const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'));
+
+let web3;
+
+if(process.argv[1].includes('mocha')) {
+    // && process.env.npm_lifecycle_event === 'test'
+    web3 = new Web3(ganache.provider());
+} else {
+    web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'));
+}
 
 const closeWeb3 = () => {
     web3.currentProvider.disconnect();
