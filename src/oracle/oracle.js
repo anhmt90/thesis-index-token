79d9b2f1af87545c44629ad3abb6b780c1b27177
T.A. Ma <anhmt90@gmail.com>
2021-08-05 11:40:04 +0200

    add announce() to oracle node to interact with the counterpart onchain

--- src/oracle/oracle.js
+++ src/oracle/oracle.js
@@ -17,6 +17,7 @@ const {
     queryUniswapPriceInEth,
     queryUniswapEthOut,
     queryUniswapTokenOut,
+    queryPortfolioEthOut,
     loadItsaTokenInfo,
     loadLastUniswapPrices,
     getAllAddrs,
@@ -24,6 +25,11 @@ const {
     getContract,
     CONTRACTS
 } = require('../utils');
+
+const {
+    ITC_EIN_V100,
+    EIN_FININS_DEFI__LENDINGSAVING
+} = require('./itc');
 
 let fundContract;
 let oracleContract;
@@ -38,19 +44,8 @@ const ETHER = web3.utils.toWei(BN(1));
 
 
 
-const setOracleGlobalVars = async () => {
-    const accounts = await web3.eth.getAccounts();
-    admin = accounts[0];
-    allAddrs = getAllAddrs();
-
-    fundContract = new web3.eth.Contract(INDEX_FUND_JSON.abi, allAddrs.indexFund);
-    const oracleAddr = await fundContract.methods.oracle().call();
-    oracleContract = new web3.eth.Contract(ORACLE_JSON.abi, oracleAddr);
-};
-
-
 const selectNewPortfolio = async () => {
-    // await fetchEthereumTokens([ITC_EIN_V100 + EIN_FININS_DEFI__LENDINGSAVING]);
+    // await fetchEthereumTokens([`${ITC_EIN_V100}=${EIN_FININS_DEFI__LENDINGSAVING}`]);
     // how to get the token contract addresses on the tokens fetched from ITSA --> Etherscan api?
 
     const itsaTokens = loadItsaTokenInfo();
@@ -74,24 +69,22 @@ const selectNewPortfolio = async () => {
     const finalTokenSet = {};
     for (const itsaToken of knownItsaTokens) {
         const sym = itsaToken.symbol.toLowerCase();
-        console.log("CURRENT KNOWN ITSA TOKENS: ", sym);
         const poolAddr = await factoryContract.methods.getPair(knownTokenSet[sym].address, allAddrs.weth).call();
-        console.log("poolAddr ===> ", poolAddr);
         if (parseInt(poolAddr) !== 0) {
             finalTokenSet[sym] = knownTokenSet[sym];
         } else {
 
         }
     }
-    console.log("FINAL TOKEN SET ===> ", Object.keys(finalTokenSet));
+    log.debug("FINAL TOKEN SET ===> ", Object.keys(finalTokenSet));
     const prevPrices = loadLastUniswapPrices();
-    console.log("PREVIOUS PRICES ===> ", prevPrices);
+    log.debug("PREVIOUS PRICES ===> ", prevPrices);
 
     const curPrices = {};
     for (const tokenSym of Object.keys(finalTokenSet)) {
         curPrices[tokenSym] = await queryUniswapPriceInEth(tokenSym);
     }
-    console.log("CURRENT PRICES ===> ", curPrices);
+    log.debug("CURRENT PRICES ===> ", curPrices);
 
     const priceDiffPercentages = [];
     for (const [symbol, curPrice] of Object.entries(curPrices)) {
@@ -103,24 +96,24 @@ const selectNewPortfolio = async () => {
         };
         priceDiffPercentages.push(diffObj);
     }
-    console.log("PRICE DIFFS ===> ", priceDiffPercentages.map(({ symbol, diffPercent }) => symbol + ': ' + diffPercent));
+    log.debug("PRICE DIFFS ===> ", priceDiffPercentages.map(({ symbol, diffPercent }) => symbol + ': ' + diffPercent));
 
     const fundContract = getContract(CONTRACTS.INDEX_FUND);
     curPortfolio = new Set(await fundContract.methods.getComponentSymbols().call());
 
     priceDiffPercentages.sort(_compareComponent);
 
-    console.log("SORTED PRICE DIFFS ===> ", priceDiffPercentages.map(({ symbol, diffPercent }) => diffPercent.toString() + '_' + symbol));
+    log.debug("SORTED PRICE DIFFS ===> ", priceDiffPercentages.map(({ symbol, diffPercent }) => diffPercent.toString() + '_' + symbol));
 
 
     // get new portfolio from based off the sorted price difference percentages
-    const newPortfolio = new Set();
+    const newPortfolio = [];
     for (const { symbol, _ } of priceDiffPercentages) {
-        if (newPortfolio.size >= curPortfolio.size)
+        if (newPortfolio.length >= curPortfolio.size)
             break;
-        newPortfolio.add(symbol);
+        newPortfolio.push(symbol);
     }
-    console.log("NEW PORTFOLIO ===> ", newPortfolio);
+    log.debug("NEW PORTFOLIO ===> ", newPortfolio);
 
     return newPortfolio;
 };
@@ -139,42 +132,50 @@ const _compareComponent = (a, b) => {
 };
 
 
-
-const decidePortfolioSubstitution = async (newPortfolio) => {
+const _deriveOutgoingAndIncomingComponents = async (newPortfolio) => {
     // get current portfolio onchain
     const fundContract = getContract(CONTRACTS.INDEX_FUND);
     const curPortfolio = (await fundContract.methods.getComponentSymbols().call()).map(component => component.toLowerCase());
-    console.log("CURRENT PORTFOLIO ===> ", curPortfolio);
+    log.debug("CURRENT PORTFOLIO ===> ", curPortfolio);
 
     // derive subtituted components (components out) from current portfolio
-    const removedComponents = new Set(curPortfolio
-        .filter(component => !newPortfolio.has(component.toLowerCase()))
-        .map(component => component.toLowerCase())
-    );
+    const newPortfolioSet = new Set(newPortfolio);
+    const componentsOut = curPortfolio
+        .filter(component => !newPortfolioSet.has(component.toLowerCase()))
+        .map(component => component.toLowerCase()
+        );
+    log.debug("REMOVED COMPONENTS ===> ", componentsOut);
 
-    console.log("REMOVED COMPONENTS ===> ", removedComponents);
-    if (removedComponents.size === 0)
+    if (componentsOut.length === 0)
         return false;
 
     // derive new components that are not in the current portfolio (components in)
     const curPortfolioSet = new Set(curPortfolio.map(component => component.toLowerCase()));
-    const newComponents = new Set([...newPortfolio].filter(component => !curPortfolioSet.has(component)));
-    console.log("NEW COMPONENTS ===> ", newComponents);
+    const componentsIn = newPortfolio.filter(component => !curPortfolioSet.has(component));
+    log.debug("NEW COMPONENTS ===> ", componentsIn);
 
+    return [componentsOut, componentsIn];
+};
+
+
+const decidePortfolioSubstitution = async (newPortfolio) => {
+    let [componentsOut, componentsIn] = await _deriveOutgoingAndIncomingComponents(newPortfolio);
+    componentsOut = new Set(componentsOut);
+    componentsIn = new Set(componentsIn);
 
     // calculate the price benefit with accounting for uniswap's fees
-    if (newComponents.size === 0 || removedComponents.size !== newComponents.size)
+    if (componentsIn.size === 0 || componentsOut.size !== componentsIn.size)
         return false;
 
     let sumEthOut = BN(0);
-    for (const symbol of removedComponents) {
+    for (const symbol of componentsOut) {
         const componentContract = getContract(symbol);
         const componentBalanceOfIndexFund = await componentContract.methods.balanceOf(allAddrs.indexFund).call();
         if (componentBalanceOfIndexFund === '0')
             continue;
 
         const ethOut = await queryUniswapEthOut(symbol, componentBalanceOfIndexFund);
-        console.log(symbol + ': ' + ethOut);
+        log.debug(symbol + ': ' + ethOut);
         sumEthOut = sumEthOut.add(BN(ethOut));
     }
 
@@ -184,18 +185,18 @@ const decidePortfolioSubstitution = async (newPortfolio) => {
      * values of the new portfolio is more significant than the uniswap's fees for swapping the old
      * components out and swapping the new components in.
      **/
-    const ethInForEach = sumEthOut.div(BN(newComponents.size));
+    const ethInForEach = sumEthOut.div(BN(componentsIn.size));
     // const ethTotalNewPortfolio = ethNetOfNewComponents.reduce((accum, ethAmount) => accum.add(BN(ethAmount)), BN(0))
     let ethTotalNewPortfolio = BN(0);
-    for (const symbol of newComponents) {
+    for (const symbol of componentsIn) {
         tokenAmountOut = await queryUniswapTokenOut(symbol, ethInForEach);
         ethAmountOut = await queryUniswapEthOut(symbol, tokenAmountOut);
         ethTotalNewPortfolio = ethTotalNewPortfolio.add(BN(ethAmountOut));
     }
-    const retainedComponents = curPortfolio.filter(component => !removedComponents.has(component));
-    console.log("RETAINED COMPONENTS ===> ", retainedComponents);
+    const componentsRetained = curPortfolio.filter(component => !componentsOut.has(component));
+    log.debug("RETAINED COMPONENTS ===> ", componentsRetained);
 
-    for (const symbol of retainedComponents) {
+    for (const symbol of componentsRetained) {
         const componentContract = getContract(symbol);
         const componentBalanceOfIndexFund = await componentContract.methods.balanceOf(allAddrs.indexFund).call();
         ethAmountOut = await queryUniswapEthOut(symbol, componentBalanceOfIndexFund);
@@ -205,9 +206,9 @@ const decidePortfolioSubstitution = async (newPortfolio) => {
     const indexTokenTotalSupply = await getContract(CONTRACTS.INDEX_TOKEN).methods.totalSupply().call();
     const indexPriceAfter = ethTotalNewPortfolio.mul(ETHER).div(BN(indexTokenTotalSupply));
 
-    const indexPriceBefore = BN(await fundContract.methods.getIndexPrice().call());
-    console.log("INDEX PRICE AFTER ===> ", indexPriceAfter.toString());
-    console.log("INDEX PRICE BEFORE ===> ", indexPriceBefore.toString());
+    const indexPriceBefore = BN(await getContract(CONTRACTS.INDEX_FUND).methods.getIndexPrice().call());
+    log.debug("INDEX PRICE AFTER ===> ", indexPriceAfter.toString());
+    log.debug("INDEX PRICE BEFORE ===> ", indexPriceBefore.toString());
 
     return indexPriceAfter.gt(indexPriceBefore);
 
@@ -216,7 +217,7 @@ const decidePortfolioSubstitution = async (newPortfolio) => {
     // take the money from the sale, buy those new components
 };
 
-const buy = async () => {
+const _buy = async () => {
     const investor = (await web3.eth.getAccounts())[2];
     await fundContract.methods.buy([]).send({
         from: investor,
@@ -233,7 +234,7 @@ const buy = async () => {
         from: investor,
         value: Ether('100'),
         gas: '5000000'
-    })
+    });
 
     await getContract(CONTRACTS.UNISWAP_ROUTER).methods.swapExactETHForTokens(
         0,
@@ -244,22 +245,66 @@ const buy = async () => {
         from: investor,
         value: Ether('100'),
         gas: '5000000'
-    })
+    });
 };
 
-const announce = async () => {
+const announce = async (allNextComponentSymbols) => {
+    const [componentSymbolsOut, componentSymbolsIn] = await _deriveOutgoingAndIncomingComponents(allNextComponentSymbols);
 
+    //get componentAddrsIn
+    const componentAddrsIn = componentSymbolsIn.map(symbol => allAddrs[symbol]);
 
+    // get componetITINs
+    const allNextComponentSymbolsSet = new Set(allNextComponentSymbols.map(symbol => symbol.toUpperCase()));
+    const itsaTokens = loadItsaTokenInfo();
+    const _itsaTokensFiltered = itsaTokens.filter(token => token[ITC_EIN_V100].startsWith(EIN_FININS_DEFI__LENDINGSAVING)
+        && allNextComponentSymbolsSet.has(token.symbol.toUpperCase())).map(token => [token.symbol.toLowerCase(), token.itin]);
+    const _itsaTokensSymbolItinMaps = Object.fromEntries(_itsaTokensFiltered);
+    // loop again with allNextComponentSymbols to get itins in the ordering of components
+    const _componentITINs = allNextComponentSymbols.map(symbol => _itsaTokensSymbolItinMaps[symbol]);
 
+    log.debug("_componentITINs ===>", _componentITINs);
+    // make _announcementMessage
+    const today = new Date();
+    const next2Days = new Date(today.setDate(today.getDate() + 2)).toUTCString();
+    const _announcementMessage = `The next portfolio update in the IndexFund contract (${allAddrs.indexFund}) will on <${next2Days} +/- 30 minutes>.`;
+    log.debug("_announcementMessage ===>", _announcementMessage);
 
+    oracleContract = getContract(CONTRACTS.ORACLE);
+    await oracleContract.methods.announce(
+        componentSymbolsOut,
+        componentAddrsIn,
+        allNextComponentSymbols,
+        _componentITINs,
+        _announcementMessage
+    ).send({
+        from: admin,
+        gas: '9000000'
+    }).on('receipt', async (txReceipt) => {
+        log.debug(`Gas used (oracle.announce()): `, txReceipt.gasUsed);
+    });
+
+    return [componentSymbolsOut, componentSymbolsIn];
+};
+
+const setOracleGlobalVars = async () => {
+    const accounts = await web3.eth.getAccounts();
+    admin = accounts[0];
+    allAddrs = getAllAddrs();
+
+    fundContract = new web3.eth.Contract(INDEX_FUND_JSON.abi, allAddrs.indexFund);
+    const oracleAddr = await fundContract.methods.oracle().call();
+    oracleContract = new web3.eth.Contract(ORACLE_JSON.abi, oracleAddr);
 };
 
 const run = async () => {
     await setOracleGlobalVars();
     const newPortfolio = await selectNewPortfolio();
-    await buy();
-    const decision = await decidePortfolioSubstitution(newPortfolio);
-    console.log('DECISON:', decision);
+    // await _buy();
+    // const decision = await decidePortfolioSubstitution(newPortfolio);
+    // log.debug('DECISON:', decision);
+
+    const [componentSymbolsOut, componentSymbolsIn] = await announce(newPortfolio);
 };
 
 
